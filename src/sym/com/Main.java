61a3ac64addb146a401185e2e8327cc9fb9560fa
Blake Olinger <blake.olinger@symbosoft.com>
2019-10-07 13:48:40 -0400

    -- reads basin config and changes basin to that config

--- src/sym/com/Main.java
+++ src/sym/com/Main.java
@@ -10,25 +10,35 @@ public class Main {
     private static final String base = "C:\\Users\\bolinger\\Documents\\SW Projects\\322326 - Prefab Blob\\";
     private static final String skeleton = "base blob - L1\\blob.basin.skeleton.txt";
     private static final String coverBasinMate = "blob - L2\\blob.cover_mates.txt";
+    private static final String basinConfigPath = "base blob - L1\\blob.basinConfig.txt";
     private static final HashMap<String, String> basinConfigTable = new HashMap<>(
             Map.of(
-                    "FB48X66S", "1"
+                    "FB48X66S", "1",
+                    "FB48X84F", "2"
             )
     );
 
     public static void main(String[] args) {
+        // read blob.basin.skeleton - user input file
         var skeletonTxt = FilesUtil.read(Paths.get(base + skeleton));
 
-        var basinMateDepth = skeletonTxt.split("\\n")[0].split(" ")[1];
+        var skeletonTextLines = skeletonTxt.split("\\n");
 
-        basinMateDepth = (Double.parseDouble(basinMateDepth) + 0.25) + "in\n";
+        // get requested basin from first line, eg; FB48X84F
+        var requestedBasin = skeletonTextLines[0].split(" ")[1].trim();
 
-        var coverBasinMateTxt = FilesUtil.read(Paths.get(base + coverBasinMate));
+        // read content from blob.basinConfig.txt
+        var basinConfigText = FilesUtil.read(Paths.get(base + basinConfigPath));
 
-        var currentBasinCoverMateDepth = coverBasinMateTxt.split("\\n")[0].split(" ")[1] + "\n";
+        // get current basin config value written to blob.basinConfig.txt
+        var currentBasinConfig = basinConfigText.split("\\n")[0].split(" ")[1].trim();
 
-        var newCoverMate = coverBasinMateTxt.replace(currentBasinCoverMateDepth, basinMateDepth);
+        // replace with requested via look-up table
+        basinConfigText = basinConfigText.replaceFirst(currentBasinConfig, basinConfigTable.get(requestedBasin));
 
-        FilesUtil.write(newCoverMate, Paths.get(base + coverBasinMate));
+        // write new basin config to blob.basin.SLDASM
+        FilesUtil.write(basinConfigText, Paths.get(base + basinConfigPath));
+
+
     }
 }
